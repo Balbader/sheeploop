8f6c39edfb43bbd169c8eec5a2ed759af6ad64b3
balbader <basilalbapro@gmail.com>
2025-11-09 13:45:33 +0100

    improve video ui

--- components/home/VideoSection.tsx
+++ components/home/VideoSection.tsx
@@ -3,8 +3,8 @@ import SheepLoopVideo from '@/videos/sheeploop-v1.mp4';
 
 export default function VideoSection() {
 	return (
-		<section className="py-16 md:py-24">
-			<div className="max-w-screen-xl mx-auto px-6 border-2 border-green-500 rounded-lg">
+		<section className="py-16 md:py-24 flex items-center justify-center">
+			<div className="max-w-screen-xl mx-auto px-6 border-2 border-green-500 rounded-lg overflow-hidden">
 				<Video src={SheepLoopVideo} playsInline />
 			</div>
 		</section>
